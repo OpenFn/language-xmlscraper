2a8cfb59a775c3440d74088a3594eee36cda7bc4
Santiago Adolfo Gutierrez Akel <sgutierrezakel@gmail.com>
2018-02-16 13:52:00 -0300

    wip

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,6 +1,9 @@
 import { execute as commonExecute, expandReferences } from 'language-common';
 import request from 'request';
 import { resolve as resolveUrl } from 'url';
+import jsdom from 'jsdom';
+const { JSDOM } = jsdom;
+import cheerio from 'cheerio';
 
 /** @module Adaptor */
 
@@ -34,12 +37,11 @@ export function execute(...operations) {
  * execute(
  *   appendValues(table, params)
  * )(state)
- * @constructor
- * @param {string} table - the table identifier
+ * @function
  * @param {object} params - data to write to the row
  * @returns {Operation}
  */
-export function postData(params) {
+export function scrape(params) {
 
   return state => {
 
@@ -49,34 +51,83 @@ export function postData(params) {
       return new Error(`Server responded with ${response.statusCode}`)
     }
 
-    const { url, body, headers } = expandReferences(params)(state);
+    const { url } = expandReferences(params)(state);
 
-    return new Promise((resolve, reject) => {
-      console.log("Request body:");
-      console.log("\n" + JSON.stringify(body, null, 4) + "\n");
-      request.post ({
+    const { username, password } = state.configuration;
+
+    const req = request.defaults({
+      jar: true,
+      followAllRedirects: true
+    });
+
+    req.post(
+      {
         url: url,
-        json: body,
-        headers
-      }, function(error, response, body){
-        error = assembleError({error, response})
-        if(error) {
-          reject(error);
-          console.log(response);
-        } else {
-          console.log("Printing response...\n");
-          console.log(JSON.stringify(response, null, 4) + "\n");
-          console.log("POST succeeded.");
-          resolve(body);
+        headers : {
+          'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36"
         }
-      })
-    }).then((data) => {
-      const nextState = { ...state, response: { body: data } };
-      return nextState;
+        ,
+        formData: {
+          "sap-user": username,
+          "sap-password": password
+        }
+      }, function(error, response, body){
+        console.log(body);
+    	if(!error && response.statusCode == 200){
+    		const dom = cheerio.load(body)
+        req.post(
+          {
+            url: "http://dpdercorda.derco.cl:8080/sap(bD1lcyZjPTMwMA==)/bc/bsp/sap/zdp_opermante/c_opermante_ingreso.do"
+            ,
+            query: {
+              "htmlbScrollX": 4,
+              "htmlbScrollY": 6,
+              "htmlbevt_ty": "htmlb:button:click:null",
+              "htmlbevt_frm": "formulario",
+              "htmlbevt_oid": "msg_header_buscar_mante",
+              "htmlbevt_id": "limpiardatos",
+              "htmlbevt_cnt": 0,
+              "onInputProcessing": "htmlb",
+              "sap-htmlb-design": "",
+              "msg_header_dd_marca": "03",
+              "msg_header_dd_modelservi": "AREN_MEGANE_III_1.6",
+              "msg_header_dd_tipo_mante": "A010"
+            }
+          }, function(error, response, body){
+            // console.log(response.headers);
+            // console.log(response.headers['set-cookie']);
+            console.log(response);
+            // console.log(body);
+            if(!error && response.statusCode == 200){
+              const dom = cheerio.load(body)
+              console.log(dom.html);
+            }
+          });
+    	}
     })
 
-  }
 
+
+
+  }
+  // curl 'http://dpdercorda.derco.cl:8080/sap(bD1lcyZjPTMwMA==)/bc/bsp/sap/zdp_opermante/c_opermante_ingreso.do' \
+  //   -H 'Cookie:
+  //   sap-contextid=SID%3aANON%3alprvsaperpa04_VMP_00%3aHgjptoN51bq8qprFbctdhy1NFkAGo89JYPx-VM1b-NEW;
+  //   sap-appcontext=c2FwLXNlc3Npb25pZD1TSUQlM2FBTk9OJTNhbHBydnNhcGVycGEwNF9WTVBfMDAlM2FIZ2pwdG9ONTFicThxcHJGYmN0ZGh5MU5Ga0FHbzg5SllQeC1WTTFiLUFUVA%3d%3d;
+  //   sap-usercontext=sap-language=S&sap-client=300;
+  //   MYSAPSSO2=AjQxMDMBABhGAFQATwBSAFIARQBTAE0AIAAgACAAIAACAAYzADAAMAADABBWAE0AUAAgACAAIAAgACAABAAYMgAwADEAOAAwADIAMQA1ADIAMAAwADYABQAEAAAACAYAAlgACQACUwD%2fAVYwggFSBgkqhkiG9w0BBwKgggFDMIIBPwIBATELMAkGBSsOAwIaBQAwCwYJKoZIhvcNAQcBMYIBHjCCARoCAQEwbzBkMQswCQYDVQQGEwJERTEcMBoGA1UEChMTU0FQIFRydXN0IENvbW11bml0eTETMBEGA1UECxMKU0FQIFdlYiBBUzEUMBIGA1UECxMLSTAwMjAyODMxMTAxDDAKBgNVBAMTA1ZNUAIHIBIBIxUlUTAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTgwMjE1MjAwNjUxWjAjBgkqhkiG9w0BCQQxFgQUamkwKZoOQUPc9XVHHkwwMvILOfcwCQYHKoZIzjgEAwQvMC0CFQCEU1dZkNwYZtSEqsGKeWWh9kEOhAIUcip3JIHG%21xnMXQC9UCgRP%21RE6ec%3d;
+  //   SAP_SESSIONID_VMP_300=1YcCYLZYjw7d5gGU49O-PMKXIh4SixHoox8AUFaUrE0%3d' \
+  //   -H 'Origin: http://dpdercorda.derco.cl:8080' \
+  //   -H 'Accept-Encoding: gzip, deflate' \
+  //   -H 'Accept-Language: en-US,en;q=0.8,es;q=0.6' \
+  //   -H 'Upgrade-Insecure-Requests: 1' \
+  //   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36' \
+  //   -H 'Content-Type: application/x-www-form-urlencoded' \
+  //   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8' \
+  //   -H 'Cache-Control: max-age=0' \
+  //   -H 'Referer: http://dpdercorda.derco.cl:8080/sap(bD1lcyZjPTMwMA==)/bc/bsp/sap/zdp_opermante/c_opermante_ingreso.do' \
+  //   -H 'Connection: keep-alive' \
+  //   --data 'htmlbScrollX=4&htmlbScrollY=6&htmlbevt_ty=htmlb%3Abutton%3Aclick%3Anull&htmlbevt_frm=formulario&htmlbevt_oid=msg_header_buscar_mante&htmlbevt_id=limpiardatos&htmlbevt_cnt=0&onInputProcessing=htmlb&sap-htmlb-design=&msg_header_dd_marca=03&msg_header_dd_modelservi=AREN_MEGANE_III_1.6&msg_header_dd_tipo_mante=A010' --compressed
 }
 
 export {
